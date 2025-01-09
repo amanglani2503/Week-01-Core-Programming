9058de12a16dce6606d709d9d23ad82b743d72e2
amanglani2503 <ambermanglani8871@gmail.com>
2025-01-09 23:52:27 +0530

    Calculating Harry's Age in 2024

---
+++ Level1/HarryAge.java
@@ -0,0 +1,8 @@
+class HarryAge{
+	public static void main(String[] args){
+		int harryBirthYear = 2000;
+		int currentYear = 2024;
+		
+		System.out.println("Harry's Age in 2024 is " + (currentYear - harryBirthYear));
+	}
+}
