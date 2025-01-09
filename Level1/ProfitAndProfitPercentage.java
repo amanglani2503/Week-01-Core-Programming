549bff584fbbcf0377ee373a3140421b83959096
amanglani2503 <ambermanglani8871@gmail.com>
2025-01-10 00:02:35 +0530

    Calculating profit and profit percentage from cost price and selling price

---
+++ Level1/ProfitAndProfitPercentage.java
@@ -0,0 +1,15 @@
+class ProfitAndProfitPercentage{
+	public static void main(String[] args){
+		int costPrice = 129;
+		int sellingPrice = 191;
+		
+		// Calculationg profit = Selling Price - Cost Price
+		int profit = sellingPrice - costPrice;
+		
+		// Calculating Profit Percentage = (profit / cost price) * 100;
+		
+		double profitPercentage = ((double)profit / (double)costPrice) * 100;
+		System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice);
+		System.out.println("The Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage);
+	}
+}
