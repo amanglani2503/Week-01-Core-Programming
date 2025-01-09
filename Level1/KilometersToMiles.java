470cb5de7c28192af152bd7456c772f673e8b76c
amanglani2503 <ambermanglani8871@gmail.com>
2025-01-09 23:56:06 +0530

    Program to convert distance from Kilometers to Miles

---
+++ Level1/KilometersToMiles.java
@@ -0,0 +1,10 @@
+class KilometersToMiles{
+	public static void main(String[] args){
+		double givenDistance = 10.8;
+		
+		// 1 Kilometer = 1.6 Miles 
+		double distanceInMiles = givenDistance * 1.6;
+		
+		System.out.println("The distance " + givenDistance + "km in miles is " + distanceInMiles);
+	}
+}
